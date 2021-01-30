295d7f672aadffb38acd5637c91a6d6767353311
Lelushia Miley <lelushiajamieson@gmail.com>
2021-01-30 22:16:48 +0000

    Updated messages.

--- CthulhuBotPy/web/index.js
+++ CthulhuBotPy/web/index.js
@@ -135,28 +135,45 @@ function processChannel(all_data){
                     channel.setAttribute("class", "text-channel");
                 }
                 channel.setAttribute("id", all_data[i][z][0]);
-                channel.innerText = "#" + all_data[i][z][1];
+                hashtag = document.createElement("i");
+                hashtag.setAttribute("class", "fas fa-hashtag fa-sm channel_hashtag");
+                channel_name = document.createElement("span");
+                channel_name.setAttribute("class", "channel_name");
+                channel_name.innerText= all_data[i][z][1];
+                // channel.innerText = "#" + all_data[i][z][1];
                 append_to_category.appendChild(channel);
+                channel.appendChild(hashtag);
+                channel.appendChild(channel_name);
             }
         }
     }
     var text_channels = document.querySelectorAll(".text-channel");
     for(var i=0;i<text_channels.length;i++){
         text_channels[i].addEventListener("click", function(event){
-            channel_id = event.target.id;
-            var remove_focus = document.querySelector(".focused");
-            remove_focus.classList.remove("focused");
-            event.target.classList.add("focused");
-            var placeholder = document.getElementById("message_box");
-            var placeholder_string = "Message " + event.target.innerText;
-            placeholder.setAttribute("placeholder", placeholder_string);
-            var remove_messages = document.getElementsByClassName("messages");
-            while(remove_messages[0]){
-                remove_messages[0].parentNode.removeChild(remove_messages[0])
+            if(event.target.className == "text-channel" || event.target.className == "channel_name" || event.target.className == "fas fa-hashtag fa-sm channel_hashtag"){
+                if(event.target.className == "text-channel"){
+                    channel_id = event.target.id;
+                    var target_el = event.target;
+                } else {
+                    channel_id = event.target.parentNode.id;
+                    var target_el = event.target.parentNode;
+                }
+                var remove_focus = document.querySelector(".focused");
+                if(target_el != remove_focus){
+                    remove_focus.classList.remove("focused");
+                    target_el.classList.add("focused");
+                    var placeholder = document.getElementById("message_box");
+                    var placeholder_string = "Message #" + target_el.innerText;
+                    placeholder.setAttribute("placeholder", placeholder_string);
+                    var remove_messages = document.getElementsByClassName("messages");
+                    while(remove_messages[0]){
+                        remove_messages[0].parentNode.removeChild(remove_messages[0])
+                    }
+                    eel.update_channel_id(channel_id);
+                    eel.update_channel(channel_id);
+                }
             }
-            eel.update_channel_id(channel_id);
-            eel.update_channel(channel_id);
-        });
+        },true);
     }
 }
 
@@ -204,72 +221,235 @@ function send_message_data(message_data){
         remove_messages[0].parentNode.removeChild(remove_messages[0])
     }
     for(var i=message_data.length-1;i>-1;i--){
-        var message = document.createElement("DIV");
-        message.setAttribute("class", "messages");
-        message.setAttribute("id",message_data[i][1]);
         var messageArea = document.getElementById("messageArea");
-        messageArea.appendChild(message);
-        var message = document.getElementById(message_data[i][1]);
-        var pfp = document.createElement("IMG");
-        pfp.setAttribute("src", message_data[i][4]);
-        pfp.setAttribute("class","userPFP");
-        var author = document.createElement("span");
-        author.setAttribute("class","messages messageUser");
-        color = message_data[i][3];
-        console.log(color);
-        author.setAttribute("style", "color: " + color);
-        author.innerText = message_data[i][2];
-        content = document.createElement("span");
-        content.setAttribute("class","messages messageContent");
-        content.innerText = message_data[i][5];
-        time = document.createElement("span");
-        time.setAttribute("class", "messages messageTime");
-        time.innerText = message_data[i][7];
-        message.appendChild(pfp);
-        message.appendChild(author);
-        message.appendChild(content);
-        message.appendChild(time);
+        if(messageArea.childNodes.length >= 1){
+            var check = 0;
+            message_id = messageArea.lastChild.id;
+            var last_message = document.getElementById(message_id);
+            last_message_id = messageArea.lastChild.lastChild.firstChild.firstChild.id;
+            var last_message_time = messageArea.lastChild.childNodes[1].childNodes[0].childNodes[1].innerText;
+            if(last_message_time == message_data[i][7]){
+                var check = 1;
+            } else if(last_message_time.startsWith("Y") && message_data[i][7].startsWith("Y")){
+                if(last_message_time.substring(13,18) == message_data[i][7].substring(13,18)) {
+                    var check = 1;
+                }
+            } else if(last_message_time.startsWith("T") && message_data[i][7].startsWith("T")){
+                if(last_message_time.substring(9,14) == message_data[i][7].substring(9,14)){
+                    var check = 1;
+                }
+            } else {
+                var check = 0;
+            }
+            if(last_message_id == message_data[i][0] && check == 1){
+                var addition_left = document.createElement("DIV");
+                addition_left.setAttribute("class", "addition_left");
+                var user_right = document.createElement("DIV");
+                user_right.setAttribute("class", "user_right");
+                last_message.appendChild(addition_left);
+                last_message.appendChild(user_right);
+                var user_content = document.createElement("DIV");
+                user_content.setAttribute("class", "user_content");
+                user_right.appendChild(user_content);
+                content = document.createElement("span");
+                content.setAttribute("class","messageContent");
+                content.setAttribute("id", message_data[i][0]);
+                content.innerText = message_data[i][5];
+                user_content.appendChild(content);
+                var element = document.getElementById("messageContainer");
+                if(message_data[8]){
+                    updateScroll();
+                } else if(element.scrollTop > element.scrollHeight-700){
+                    updateScroll();
+                }
+            } else {
+                var message = document.createElement("DIV");
+                message.setAttribute("class", "messages " + message_data[i][0]);
+                message.setAttribute("id",message_data[i][1]);
+                var messageArea = document.getElementById("messageArea");
+                messageArea.appendChild(message);
+                var message = document.getElementById(message_data[i][1]);
+                var pfp = document.createElement("IMG");
+                pfp.setAttribute("src", message_data[i][4]);
+                pfp.setAttribute("class","userPFP");
+                var author = document.createElement("span");
+                author.setAttribute("class","messageUser");
+                color = message_data[i][3];
+                author.setAttribute("style", "color: " + color);
+                author.setAttribute("id", message_data[i][0]);
+                author.innerText = message_data[i][2];
+                content = document.createElement("span");
+                content.setAttribute("class","messageContent");
+                content.innerText = message_data[i][5];
+                time = document.createElement("span");
+                time.setAttribute("class", "messageTime");
+                time.innerText = message_data[i][7];
+                var user_left = document.createElement("DIV");
+                user_left.setAttribute("class", "user_left");
+                message.appendChild(user_left);
+                var user_right = document.createElement("DIV");
+                user_right.setAttribute("class", "user_right");
+                message.appendChild(user_right);
+    
+                var user_icon = document.createElement("DIV");
+                user_icon.setAttribute("class", "user_icon");
+                user_left.appendChild(user_icon);
+                var user_and_time = document.createElement("DIV");
+                user_and_time.setAttribute("class", "user_and_time");
+                user_right.appendChild(user_and_time);
+                var user_content = document.createElement("DIV");
+                user_content.setAttribute("class", "user_content");
+                user_right.appendChild(user_content);
+    
+                user_icon.appendChild(pfp);
+                user_and_time.appendChild(author);
+                user_and_time.appendChild(time);
+                user_content.appendChild(content);
+            }
+        } else {
+            var message = document.createElement("DIV");
+            message.setAttribute("class", "messages " + message_data[i][0]);
+            message.setAttribute("id",message_data[i][1]);
+            var messageArea = document.getElementById("messageArea");
+            messageArea.appendChild(message);
+            var message = document.getElementById(message_data[i][1]);
+            var pfp = document.createElement("IMG");
+            pfp.setAttribute("src", message_data[i][4]);
+            pfp.setAttribute("class","userPFP");
+            var author = document.createElement("span");
+            author.setAttribute("class","messageUser");
+            color = message_data[i][3];
+            author.setAttribute("style", "color: " + color);
+            author.setAttribute("id", message_data[i][0]);
+            author.innerText = message_data[i][2];
+            content = document.createElement("span");
+            content.setAttribute("class","messageContent");
+            content.innerText = message_data[i][5];
+            time = document.createElement("span");
+            time.setAttribute("class", "messageTime");
+            time.innerText = message_data[i][7];
+            var user_left = document.createElement("DIV");
+            user_left.setAttribute("class", "user_left");
+            message.appendChild(user_left);
+            var user_right = document.createElement("DIV");
+            user_right.setAttribute("class", "user_right");
+            message.appendChild(user_right);
+
+            var user_icon = document.createElement("DIV");
+            user_icon.setAttribute("class", "user_icon");
+            user_left.appendChild(user_icon);
+            var user_and_time = document.createElement("DIV");
+            user_and_time.setAttribute("class", "user_and_time");
+            user_right.appendChild(user_and_time);
+            var user_content = document.createElement("DIV");
+            user_content.setAttribute("class", "user_content");
+            user_right.appendChild(user_content);
+
+            user_icon.appendChild(pfp);
+            user_and_time.appendChild(author);
+            user_and_time.appendChild(time);
+            user_content.appendChild(content);
+        }
     }
+
+    var allAuthors = document.getElementsByClassName("messageUser");
+    for(i=0;i<allAuthors.length;i++){
+        allAuthors[i].addEventListener("mouseover", event => {
+            color = event.target.style.color;
+            event.target.setAttribute("style", "color: " + color + "; " + "border-bottom: 1px solid " + color + ";");
+        });
+        allAuthors[i].addEventListener("mouseout", event => {
+            color = event.target.style.color;
+            event.target.setAttribute("style", "color: " + color + ";");
+        });
+    }
+
     updateScroll();
 }
 
 eel.expose(new_message);
 function new_message(message_data){
     var focused_channel = document.querySelector(".focused");
+    console.log("It starts");
     if(focused_channel.id == message_data[6]){
-        var message = document.createElement("DIV");
-        message.setAttribute("class", "messages");
-        message.setAttribute("id",message_data[1]);
         var messageArea = document.getElementById("messageArea");
-        messageArea.appendChild(message);
-        var message = document.getElementById(message_data[1]);
-        var pfp = document.createElement("IMG");
-        pfp.setAttribute("src", message_data[4]);
-        pfp.setAttribute("class","userPFP");
-        var author = document.createElement("span");
-        author.setAttribute("class","messages messageUser");
-        color = message_data[3];
-        console.log(color);
-        author.setAttribute("style", "color: " + color);
-        author.innerText = message_data[2];
-        content = document.createElement("span");
-        content.setAttribute("class","messages messageContent");
-        content.innerText = message_data[5];
-        time = document.createElement("span");
-        time.setAttribute("class", "messages messageTime");
-        time.innerText = message_data[7];
-        message.appendChild(pfp);
-        message.appendChild(author);
-        message.appendChild(content);
-        message.appendChild(time);
-        var element = document.getElementById("messageContainer");
-        console.log(element.scrollTop);
-        console.log(element.scrollHeight);
-        if(message_data[8]){
-            updateScroll();
-        } else if(element.scrollTop > element.scrollHeight-600){
-            updateScroll();
+        message_id = messageArea.lastChild.id;
+        var last_message = document.getElementById(message_id);
+        last_message_id = messageArea.lastChild.lastChild.firstChild.firstChild.id;
+        if(last_message_id == message_data[0]){
+            console.log("Hi");
+            var addition_left = document.createElement("DIV");
+            addition_left.setAttribute("class", "addition_left");
+            var user_right = document.createElement("DIV");
+            user_right.setAttribute("class", "user_right");
+            last_message.appendChild(addition_left);
+            last_message.appendChild(user_right);
+            var user_content = document.createElement("DIV");
+            user_content.setAttribute("class", "user_content");
+            user_right.appendChild(user_content);
+            content = document.createElement("span");
+            content.setAttribute("class","messageContent");
+            content.setAttribute("id", message_data[0]);
+            content.innerText = message_data[5];
+            user_content.appendChild(content);
+            var element = document.getElementById("messageContainer");
+            if(message_data[8]){
+                updateScroll();
+            } else if(element.scrollTop > element.scrollHeight-700){
+                updateScroll();
+            }
+        } else {
+            console.log("Down here!");
+            var message = document.createElement("DIV");
+            message.setAttribute("class", "messages");
+            message.setAttribute("id",message_data[1]);
+            var messageArea = document.getElementById("messageArea");
+            messageArea.appendChild(message);
+            var message = document.getElementById(message_data[1]);
+            var pfp = document.createElement("IMG");
+            pfp.setAttribute("src", message_data[4]);
+            pfp.setAttribute("class","userPFP");
+            var author = document.createElement("span");
+            author.setAttribute("class","messageUser");
+            color = message_data[3];
+            console.log(color);
+            author.setAttribute("style", "color: " + color);
+            author.setAttribute("id", message_data[0]);
+            author.innerText = message_data[2];
+            content = document.createElement("span");
+            content.setAttribute("class","messageContent");
+            content.innerText = message_data[5];
+            time = document.createElement("span");
+            time.setAttribute("class", "messageTime");
+            time.innerText = message_data[9];
+
+            var user_left = document.createElement("DIV");
+            user_left.setAttribute("class", "user_left");
+            message.appendChild(user_left);
+            var user_right = document.createElement("DIV");
+            user_right.setAttribute("class", "user_right");
+            message.appendChild(user_right);
+
+            var user_icon = document.createElement("DIV");
+            user_icon.setAttribute("class", "user_icon");
+            user_left.appendChild(user_icon);
+            var user_and_time = document.createElement("DIV");
+            user_and_time.setAttribute("class", "user_and_time");
+            user_right.appendChild(user_and_time);
+            var user_content = document.createElement("DIV");
+            user_content.setAttribute("class", "user_content");
+            user_right.appendChild(user_content);
+
+            user_icon.appendChild(pfp);
+            user_and_time.appendChild(author);
+            user_and_time.appendChild(time);
+            user_content.appendChild(content);
+            var element = document.getElementById("messageContainer");
+            if(message_data[8]){
+                updateScroll();
+            } else if(element.scrollTop > element.scrollHeight-700){
+                updateScroll();
+            }
         }
-        // updateScroll();
     }
 }
